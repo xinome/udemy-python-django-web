fce8c937c561715ce3ee3c7b4f4be7294f64f542
xinome <xinome2038@gmail.com>
2023-11-28 17:19:31 +0900

    119. URLディスパッチ

--- first_project/first_app/urls.py
+++ first_project/first_app/urls.py
@@ -5,4 +5,6 @@ app_name = 'first_app'
 
 urlpatterns = [
   path('hello', views.index, name='index'),
+  path('page/<str:user_name>', views.user_page, name='user_page'),
+  path('number_page/<int:number>', views.number_page, name='number_page'),
 ]
--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -6,3 +6,9 @@ from django.http import HttpResponse
 def index(request):
   return HttpResponse("<h1>Hello</h1>")
 
+def user_page(request, user_name):
+  return HttpResponse(f"<h1>{user_name}\'s page</h1>")
+
+def number_page(request, number):
+  return HttpResponse(f"<h1>page number = {number}</h1>")
+
