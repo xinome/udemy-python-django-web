b8a05d245cf8d2bd1323fd6ada95838ab8813d7c
xinome <xinome2038@gmail.com>
2023-11-28 11:09:32 +0900

    98. プロジェクト作成　言語・タイムゾーン更新

--- first_project/first_project/settings.py
+++ first_project/first_project/settings.py
@@ -103,12 +103,14 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tokyo'
 
 USE_I18N = True
 
+# UTCにして保存した場合は、表示の際に現地のタイムゾーンに変換してくれる
+# マルチ言語対応の場合は、Trueにする
 USE_TZ = True
 
 
