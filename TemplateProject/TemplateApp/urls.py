c0672166ed124bd8bbc0d84b7e2f2ffffae422c2
xinome <xinome2038@gmail.com>
2023-11-29 14:39:03 +0900

    129. Templateの継承

--- TemplateProject/TemplateApp/urls.py
+++ TemplateProject/TemplateApp/urls.py
@@ -6,4 +6,6 @@ app_name = 'template_app'
 urlpatterns = [
   path('', views.index, name='index'),
   path('home/', views.home, name='home'),
+  path('sample1/', views.sample1, name='sample1'),
+  path('sample2/', views.sample2, name='sample2'),
 ]
--- TemplateProject/TemplateApp/views.py
+++ TemplateProject/TemplateApp/views.py
@@ -19,3 +19,9 @@ def home(request):
     'favorite_fruits': favorite_fruits,
     'my_info': my_info,
   })
+
+def sample1(request):
+  return render(request, 'sample1.html')
+
+def sample2(request):
+  return render(request, 'sample2.html')
