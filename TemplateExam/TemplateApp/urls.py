047600ac0f9ebb8beabfb08def3fb7e5626910d7
xinome <xinome2038@gmail.com>
2023-11-29 18:30:55 +0900

    表示時の修正

--- TemplateExam/TemplateApp/urls.py
+++ TemplateExam/TemplateApp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 
-app_name = 'TemplateApp'
+app_name = 'app'
 urlpatterns = [
   path('home', views.home, name='home'),
   path('members', views.members, name='members'),
--- TemplateExam/TemplateApp/views.py
+++ TemplateExam/TemplateApp/views.py
@@ -15,21 +15,21 @@ member_list = [
   Member(0, 'Taro', '2021/04/01', 'img/taro.jpg'),
   Member(1, 'Jiro', '2022/04/01', 'img/jiro.jpg'),
   Member(2, 'Hanako', '2023/04/01', 'img/hanako.jpg'),
-  Mmeber(3, 'Yoshiko', '2024/04/01', 'img/yoshiko.jpg'),
+  Member(3, 'Yoshiko', '2024/04/01', 'img/yoshiko.jpg'),
 ]
 
 # ホーム画面
 def home(request):
-    return render(request, 'home.html')
+  return render(request, 'home.html')
 
 # メンバー一覧画面
 def members(request):
-    return render(request, 'members.html', context={
-      'members': member_list
-    })
+  return render(request, 'members.html', context={
+    'members': member_list
+  })
 
 # メンバー詳細画面
 def member(request, id):
-    return render(request, 'member_detail.html', context={
-      'member': member_list[id]
-    })
+  return render(request, 'member_detail.html', context={
+    'member': member_list[id]
+  })
