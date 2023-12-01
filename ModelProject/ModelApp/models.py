29c169088f4c37d3b8519779d04de978a093ccfd
xinome <xinome2038@gmail.com>
2023-12-01 18:01:07 +0900

    152. Modelでの他のテーブルとの紐づけ（外部キー）～その１～

--- ModelProject/ModelApp/models.py
+++ ModelProject/ModelApp/models.py
@@ -28,3 +28,24 @@ class Person(BaseMeta):
   def __str__(self):
     return f'{self.first_name} {self.last_name}'
   
+class Students(models.Model):
+  name = models.CharField(max_length=20)
+  age = models.IntegerField()
+  major = models.CharField(max_length=20)
+  school = models.ForeignKey('Schools', on_delete=models.CASCADE)
+
+  class Meta:
+    db_table = 'students'
+
+class Schools(models.Model):
+  name = models.CharField(max_length=20)
+  prefecuture = models.ForeignKey('Prefectures', on_delete=models.CASCADE)
+
+  class Meta:
+    db_table = 'schools'
+
+class Prefectures(models.Model):
+  name = models.CharField(max_length=20)
+
+  class Meta:
+    db_table = 'prefectures'
