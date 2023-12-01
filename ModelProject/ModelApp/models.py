165da8456339f4da020eb37f9633270bd3461b55
xinome <xinome2038@gmail.com>
2023-12-01 15:11:25 +0900

    144. Modelのマイグレーション

--- ModelProject/ModelApp/models.py
+++ ModelProject/ModelApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+# classはテーブルを作成するためのクラス
+class Person(models.Model):
+  first_name = models.CharField(max_length=30)
+  last_name = models.CharField(max_length=30)
+
+class Sales(models.Model):
+   fee = models.IntegerField()
+   created_at = models.DateTimeField()
+   updated_at = models.DateTimeField()
