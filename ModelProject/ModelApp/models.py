de96cc61a8bfc37a34a3703e212cfa2bc9ead6b2
xinome <xinome2038@gmail.com>
2023-12-01 15:46:15 +0900

    146. Modelのフィールド作成

--- ModelProject/ModelApp/models.py
+++ ModelProject/ModelApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
@@ -6,8 +7,10 @@ from django.db import models
 class Person(models.Model):
   first_name = models.CharField(max_length=30)
   last_name = models.CharField(max_length=30)
-
-class Sales(models.Model):
-   fee = models.IntegerField()
-   created_at = models.DateTimeField()
-   updated_at = models.DateTimeField()
+  birthday = models.DateField(default='1900-01-01')
+  email = models.EmailField(db_index=True)  # db_index=Trueでインデックスを作成
+  salary = models.FloatField(null=True)
+  memo = models.TextField()
+  web_site = models.URLField(null=True, blank=True)  # null=TrueでNULLを許可, blank=Trueで空文字を許可
+  create_at = models.DateTimeField(default=timezone.now)
+  
