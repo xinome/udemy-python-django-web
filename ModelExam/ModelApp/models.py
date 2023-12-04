7741514a7d53ecca48b5be2271a643732e9ac4ce
xinome <xinome2038@gmail.com>
2023-12-04 19:23:07 +0900

    163. Modelを用いた演習～その１～　テーブル定義、マイグレーション

--- ModelExam/ModelApp/models.py
+++ ModelExam/ModelApp/models.py
@@ -1,3 +1,44 @@
 from django.db import models
 
 # Create your models here.
+
+class Tests(models.Model):
+  name = models.CharField(max_length=50)
+
+  class Meta:
+    db_table = 'tests'
+
+
+class TestResults(models.Model):
+  student_id = models.ForeignKey(
+    'Students',
+    on_delete=models.CASCADE
+  )
+  test_id = models.ForeignKey(
+    'Tests',
+    on_delete=models.CASCADE
+  )
+  score = models.IntegerField()
+
+  class Meta:
+    db_table = 'test_results'
+
+
+class Students(models.Model):
+  class_id = models.ForeignKey(
+    'Classes',
+    on_delete=models.CASCADE
+  )
+  name = models.CharField(max_length=50)
+  grade = models.IntegerField()
+
+  class Meta:
+    db_table = 'students'
+
+
+class Classes(models.Model):
+  name = models.CharField(max_length=10)
+
+  class Meta:
+    db_table = 'classes'
+
--- ModelExam/ModelExam/settings.py
+++ ModelExam/ModelExam/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ModelApp',
 ]
 
 MIDDLEWARE = [
@@ -103,9 +104,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ja'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Tokyo/Asia'
 
 USE_I18N = True
 
