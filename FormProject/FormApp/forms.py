be24950e779587f8cbeaa5af4cee7bb638e6920e
xinome <xinome2038@gmail.com>
2023-12-07 16:35:22 +0900

    173. FormのフィールドにID, Classを追加してレイアウト変更

--- FormProject/FormApp/forms.py
+++ FormProject/FormApp/forms.py
@@ -5,7 +5,7 @@ class UserInfo(forms.Form):
   age = forms.IntegerField(label='年齢')
   mail = forms.EmailField(
     label='メールアドレス',
-    widget=forms.TextInput(attrs={'placeholder': 'sample@mail.com'})
+    widget=forms.TextInput(attrs={'class': 'mail_class', 'placeholder': 'sample@mail.com'})
   )
 
   is_married = forms.BooleanField(initial=True)
@@ -19,3 +19,9 @@ class UserInfo(forms.Form):
   ), widget=forms.CheckboxSelectMultiple)
   homepage = forms.URLField(required=False)
   memo = forms.CharField(widget=forms.Textarea, required=False)
+
+# コンストラクタでスタイルを指定
+def __init__(self, *args, **kwargs):
+  super(UserInfo, self).__init__(*args, **kwargs)
+  self.fields['job'].widget.attrs['id'] = 'id_job'
+  self.fields['hobbies'].widget.attrs['class'] = 'class_hobbies'
--- FormProject/FormProject/settings.py
+++ FormProject/FormProject/settings.py
@@ -17,6 +17,7 @@ import os
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 TEMPLATE_DIR = os.path.join(BASE_DIR,'templates')
+STATIC_DIR = os.path.join(BASE_DIR,'static')
 
 
 # Quick-start development settings - unsuitable for production
@@ -120,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    STATIC_DIR,
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
