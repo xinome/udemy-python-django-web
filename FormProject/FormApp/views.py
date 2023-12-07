327e6d8f965c972c26f892d353c40c75b62cd837
xinome <xinome2038@gmail.com>
2023-12-07 15:37:25 +0900

    170. DjangoのFormの実装～その２～

--- FormProject/FormApp/views.py
+++ FormProject/FormApp/views.py
@@ -8,6 +8,16 @@ def index(request):
 
 def form_page(request):
   form = forms.UserInfo()
+  if request.method == 'POST':
+    # Formで送られたデータを取得
+    form = forms.UserInfo(request.POST)
+    # バリデーションチェック
+    if form.is_valid():
+      print('バリデーション成功')
+      print(
+        f'name: {form.cleaned_data["name"]}, mail: {form.cleaned_data["mail"]}, age: {form.cleaned_data["age"]}'
+      )
+
   return render(request, 'formapp/form_page.html', context={
     'form': form
   })
