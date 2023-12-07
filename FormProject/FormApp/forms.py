d07bf7ca9189fe8d42f357c7bf44fb3fb1be3414
xinome <xinome2038@gmail.com>
2023-12-07 15:58:11 +0900

    171. Formのフィールド一覧

--- FormProject/FormApp/forms.py
+++ FormProject/FormApp/forms.py
@@ -4,3 +4,14 @@ class UserInfo(forms.Form):
   name = forms.CharField()
   age = forms.IntegerField()
   mail = forms.EmailField()
+
+  is_married = forms.BooleanField()
+  birthday = forms.DateField()
+  salary = forms.DecimalField()
+  job = forms.ChoiceField(choices=(
+    (1, '正社員'), (2, '自営業'), (3, '学生'), (4, '無職')
+  ))
+  hobby = forms.MultipleChoiceField(choices=(
+    (1, 'スポーツ'), (2, '読書'), (3, '映画鑑賞'), (4, 'その他')
+  ))
+  homepage = forms.URLField()
--- FormProject/FormApp/views.py
+++ FormProject/FormApp/views.py
@@ -11,12 +11,14 @@ def form_page(request):
   if request.method == 'POST':
     # Formで送られたデータを取得
     form = forms.UserInfo(request.POST)
+
     # バリデーションチェック
     if form.is_valid():
-      print('バリデーション成功')
-      print(
-        f'name: {form.cleaned_data["name"]}, mail: {form.cleaned_data["mail"]}, age: {form.cleaned_data["age"]}'
-      )
+      # print('バリデーション成功')
+      # print(
+      #   f'name: {form.cleaned_data["name"]}, mail: {form.cleaned_data["mail"]}, age: {form.cleaned_data["age"]}'
+      # )
+      print(form.cleaned_data)
 
   return render(request, 'formapp/form_page.html', context={
     'form': form
