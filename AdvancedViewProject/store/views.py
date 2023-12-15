742227c63a0909f96bd1c3342baa3b8905ff00a1
xinome <xinome2038@gmail.com>
2023-12-15 15:46:09 +0900

    199. リダイレクトの実行～その３～

--- AdvancedViewProject/store/views.py
+++ AdvancedViewProject/store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Items
 
 # Create your views here.
@@ -14,3 +14,10 @@ def item_detail(request, id):
   return render(request, 'store/item_detail.html', context={
     'item': item
   })
+
+def to_google(request):
+  return redirect('https://www.google.com/')
+
+# 特定の商品の詳細ページにリダイレクトする
+def one_item(request):
+  return redirect('store:item_detail', id=1)
