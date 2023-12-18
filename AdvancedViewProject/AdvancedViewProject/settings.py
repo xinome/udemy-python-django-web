6e8afebab3c4a83cd9cf6b03b499b4bdd3116874
xinome <xinome2038@gmail.com>
2023-12-18 15:04:28 +0900

    207. ログイン処理の基本～その３～

--- AdvancedViewProject/AdvancedViewProject/settings.py
+++ AdvancedViewProject/AdvancedViewProject/settings.py
@@ -27,7 +27,7 @@ STATIC_DIR = os.path.join(BASE_DIR,'static')
 SECRET_KEY = 'django-insecure-d*+v-!uz*c@kl2)x*7kzik^!+=q-9mca2gjsp%vgr$f30r-d^+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
