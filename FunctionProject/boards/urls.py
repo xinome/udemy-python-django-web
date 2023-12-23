04865afbaad9787e194e6db9dadf3219c23a0fef
xinome <xinome2038@gmail.com>
2023-12-23 17:22:31 +0900

    240. 掲示板画面の作成～テーマ編集画面の作成～

--- FunctionProject/boards/urls.py
+++ FunctionProject/boards/urls.py
@@ -6,4 +6,5 @@ app_name = 'boards'
 urlpatterns = [
   path('create_theme', views.create_theme, name='create_theme'),
   path('list_themes', views.list_themes, name='list_themes'),
+  path('edit_theme/<int:id>', views.edit_theme, name='edit_theme'),
 ]
--- FunctionProject/boards/views.py
+++ FunctionProject/boards/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from . import forms
 from django.contrib import messages
 from .models import Themes
+from django.http import Http404
 
 # Create your views here.
 def create_theme(request):
@@ -21,4 +22,20 @@ def list_themes(request):
   themes = Themes.objects.fetch_all_themes()
   return render(request, 'boards/list_themes.html', context={
     'themes': themes
+  })
+
+def edit_theme(request, id):
+  theme = get_object_or_404(Themes, id=id)
+  if theme.user_id != request.user.id:
+    raise Http404
+  
+  edit_theme_form = forms.CreateThemeForm(request.POST or None, instance=theme)
+  if edit_theme_form.is_valid():
+    edit_theme_form.save()
+    messages.success(request, '掲示板を更新しました。')
+    return redirect('boards:list_themes')
+  
+  return render(request, 'boards/edit_theme.html', context={
+    'edit_theme_form': edit_theme_form,
+    'id': id,
   })
