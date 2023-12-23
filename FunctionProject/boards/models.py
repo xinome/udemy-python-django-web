f4e8c95c6cdc51f5a7d30df0f8f00eedbb83c6cf
xinome <xinome2038@gmail.com>
2023-12-23 16:46:05 +0900

    238. 掲示板画面の作成～テーマ作成画面の作成～

--- FunctionProject/boards/models.py
+++ FunctionProject/boards/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Themes(models.Model):
-  theme = models.CharField(max_length=255)
+  title = models.CharField(max_length=255)
   user = models.ForeignKey(
     'accounts.Users', on_delete=models.CASCADE
   )
--- FunctionProject/boards/views.py
+++ FunctionProject/boards/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import forms
+from django.contrib import messages
 
 # Create your views here.
 def create_theme(request):
-  create_theme_form = CreateThemeForm(request.POST or None)
+  create_theme_form = forms.CreateThemeForm(request.POST or None)
+  if create_theme_form.is_valid():
+    # ここでユーザーをセットする
+    create_theme_form.instance.user = request.user
+    create_theme_form.save()
+    messages.success(request, '掲示板を作成しました。')
+    return redirect('accounts:home')
 
-  return render(request, 'boards/create_theme.html')
+  return render(request, 'boards/create_theme.html', context={
+    'create_theme_form': create_theme_form
+  })
+
