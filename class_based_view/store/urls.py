d5b8c4087a71a3e9455bbac22953b295714547e9
xinome <xinome2038@gmail.com>
2024-02-07 16:32:15 +0900

    252. TemplateViewの実装

--- class_based_view/store/urls.py
+++ class_based_view/store/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, HomeView
+from django.views.generic.base import TemplateView
 
 app_name = 'store'
 
 urlpatterns = [
   path('index/', IndexView.as_view(), name='index'),
+  # path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
+  path('home/<name>', HomeView.as_view(), name='home'),
 ]
--- class_based_view/store/views.py
+++ class_based_view/store/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.views.generic.base import (
-  View,
+  View, TemplateView
 )
 from . import forms
+from datetime import datetime
 
 # Create your views here.
 
@@ -18,8 +19,18 @@ class IndexView(View):
     book_form = forms.BookForm(request.POST)
     if book_form.is_valid():
       book_form.save()
-      
+
     return render(request, 'index.html', context={
       'book_form': book_form,
     })
 
+class HomeView(TemplateView):
+  template_name = 'home.html'
+  
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    print(kwargs)
+    context['name'] = kwargs.get('name')
+    context['time'] = datetime.now()
+    
+    return context
